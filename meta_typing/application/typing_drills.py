ff63554e8bc547caaf4cbfcd65bfc1d437bf9997
Jonathan Wamsley <jonnywamsley@gmail.com>
2020-08-02 16:48:55 -0400

    changed layout loading files instead of computing word lists

--- meta_typing/application/typing_drills.py
+++ meta_typing/application/typing_drills.py
@@ -13,19 +13,18 @@ class TypingDrills:
     def get_exercise_menu(self):
         '''Additional Exercises are named here with a below name_drill function'''
         menu = [
-            'Bigraphs',
-            'Trigraphs',
-            'Speed Drill',
+            'Ngraphs',
             'Word Bank',
+            'Speed Drill'
         ]
         return menu
 
     def start_up(self):
         '''combines all the typing components to return a word drill list'''
         exercise_type = self.get_exercise_type()
-        additional_parameters = self.ask_for_additional_paramters()
-        word_drill = self.get_exercise(exercise_type, additional_parameters)
-        return word_drill
+        exercise_word_drill = self.get_exercise(exercise_type)
+        customized_word_drill = self.ask_for_additional_paramters(exercise_word_drill)
+        return customized_word_drill
 
     def get_exercise_type(self):
         '''selection window is created to get the exercise type from user'''
@@ -33,35 +32,83 @@ class TypingDrills:
         selected_exercise_response = exercise_types_window.get_selected_response() # should I get the row or the string value
         return selected_exercise_response
 
-    def ask_for_additional_paramters(self):
+    def get_exercise(self, exercise_type):
+        if exercise_type == 'Ngraphs':
+            n_graph_response = self.n_graphs()
+            words = self.get_n_graph(n_graph_response)
+        elif exercise_type == 'Word Bank':
+            word_bank_response = self.word_bank()
+            words = self.get_word_bank(word_bank_response)
+        elif exercise_type == 'Speed Drill':
+            words = self.apply_speed_drill()
+        return words
+
+    def n_graphs(self):
+        n_graph_menu = ['Bigraphs', 'Trigraphs', 'Tetragraphs', 'Bigrams']
+        n_graph_window = SelectionWindow(self.stdscr, selection_list = n_graph_menu)
+        selected_n_graph_response = n_graph_window.get_selected_response()
+        return selected_n_graph_response
+
+    def get_n_graph(self, n_graph_response):
+        if n_graph_response == 'Bigraphs':
+            word_list = self.get_word_list('bigraphs')
+        elif n_graph_response == 'Trigraphs':
+            word_list = self.get_word_list('trigraphs')
+        elif n_graph_response == 'Tetragraphs':
+            word_list = self.get_word_list('tetragraphs')
+        elif n_graph_response == 'Bigrams':
+            word_list = self.get_word_list('2grams_top_1000')
+        return word_list
+
+    def word_bank(self):
+        word_bank_menu = ['Top100', 'Top300', 'Top600', 'Top1000', 'TenFastFingers']
+        word_bank_window = SelectionWindow(self.stdscr, selection_list = word_bank_menu)
+        word_bank_response = word_bank_window.get_selected_response() # should I get the row or the string value
+        return word_bank_response
+
+    def get_word_bank(self, word_bank_response):
+        if word_bank_response == 'Top100':
+            word_list = self.get_word_list('top100')
+        elif word_bank_response == 'Top300':
+            word_list = self.get_word_list('top300')
+        elif word_bank_response == 'Top600':
+            word_list = self.get_word_list('top600')
+        elif word_bank_response == 'Top10000':
+            word_list = self.get_word_list('top1000')
+        elif word_bank_response == 'TenFastFingers':
+            word_list = self.get_word_list('tenfastfingers')
+        return word_list
+
+    def get_word_list(self, file_name): # str -> list
+        '''Opens a file and returns the contents'''
+        path = f'../word_drills/{file_name}.txt'
+        data = []
+        with open(path) as f:
+            lines = f.readlines()
+            for line in lines:
+                data.append(line.rstrip())
+        return data
+
+    def ask_for_additional_paramters(self, word_list):
         '''If the user wants to tailor his exercise, additional options will appear'''
         question = 'Would you like to customize the exercise?'
         options = ['No', 'Yes']
         boolean_window = SelectionWindow(self.stdscr, static_message = question, selection_list = options)
         response = boolean_window.get_selected_row()
         if response == 0: # selected no for additional parameters
-            return None
+            return word_list
         else:
-            return self.get_additional_parameters()
+            return self.get_additional_parameters(word_list)
 
-    def get_additional_parameters(self):
+    def get_additional_parameters(self, word_list):
         '''prompts user for additional paramters'''
-        file_name = self.get_word_list_file_name() # returns name_of_word_list.txt
         word_amount = self.get_word_amount() # an integer
         randomize = self.randomization() # 0/1
-        additional_paramters = [file_name, word_amount, randomize]
-        return additional_paramters
-
-    def get_word_list_file_name(self):
-        '''prompts user to select word list file name under the data source'''
-        word_list_file_names = self.get_word_list_file_names()
-        word_list_file_names_window = SelectionWindow(self.stdscr, selection_list = word_list_file_names)
-        word_list_file_name = word_list_file_names_window.get_selected_response()
-        return word_list_file_name
-
-    def get_word_list_file_names(self):
-        '''gets filenames where the word lists are stored'''
-        return [file for file in os.listdir("../data/") if file.endswith(".txt")]
+        if randomize == 1:
+            word_list = self.randomize_word(word_list, word_amount)
+        else:
+            word_list = self.apply_word_amount(word_list, word_amount)
+        return word_list
 
     def get_word_amount(self):
         '''prompts user for an integer for word amount'''
@@ -78,80 +125,15 @@ class TypingDrills:
         boolean_window = SelectionWindow(self.stdscr, static_message = question, selection_list = options)
         return boolean_window.get_selected_row()
 
-    def get_exercise(self, exercise_type, exercise_parameters):
-        '''applies the exercise types and parameters to created a word_drill'''
-        if exercise_parameters == None:
-            file_name, word_amount, randomization = '10000words.txt', 100, 0
-        else:
-            file_name, word_amount, randomization = exercise_parameters
-        
-        words = self.get_word_list(file_name)
-        words_filtered_1 = self.apply_word_drill(words, exercise_type)
-        if randomization == 1:
-            words_filtered_2 = self.randomize_word(words_filtered_1, word_amount)
-        else:
-            words_filtered_2 = self.apply_word_amount(words_filtered_1, word_amount)
-        return words_filtered_2
-
-    def get_word_list(self, file_name): # str -> list
-        '''Opens a file and returns the contents'''
-        path = f'../data/{file_name}'
-        with open(path) as f:
-            lines = f.readlines()
-            words_list = []
-            for line in lines:
-                words_list.append(line.rstrip())
-        return words_list
-
-    def apply_word_drill(self, words_list, exercise_type):
-        '''applies the word drill function'''
-        if exercise_type == 'Bigraphs':
-            word_drill = self.apply_bigraphs_drill(words_list)
-        elif exercise_type == 'Trigraphs':
-            word_drill = self.apply_trigraphs_drill(words_list)
-        elif exercise_type == 'Speed Drill':
-            word_drill = self.apply_speed_drill(words_list)
-        elif exercise_type == 'Word Bank': # pass
-            word_drill = words_list
-        return word_drill
-
-    def apply_bigraphs_drill(self, word_list):
-        alpha = [char for char in 'qwertasdfgzxcvbjuiophyklnm']
-        two_combo = []
-        for char1 in alpha:
-            for char2 in alpha:
-                two_combo.append(char1 + char2)
-
-        two_char_counter = Counter()
-        for word in word_list:
-            for combo in two_combo:
-                if combo in word:      
-                    two_char_counter[combo] += 1
-        return [digram[0] for digram in two_char_counter.most_common()]
-
-    def apply_trigraphs_drill(self, word_list):
-        alpha = [char for char in 'qwertasdfgzxcvbjuiophyklnm']
-        three_combo = []
-        for char1 in alpha:
-            for char2 in alpha:
-                for char3 in alpha:
-                    three_combo.append(char1 + char2 + char3)
-
-        three_char_counter = Counter()
-        for word in word_list:
-            for combo in three_combo:
-                if combo in word:      
-                    three_char_counter[combo] += 1
-        return [trigram[0] for trigram in three_char_counter.most_common()]
-
-    def apply_speed_drill(self, word_list):
-        words = random.choices(word_list[:300], k=14)
+    def apply_speed_drill(self):
+        word_list = self.get_word_list('top100')
+        words = random.choices(word_list[:300], k=16)
         exercise_words = []
         word_q = []
         while words:
             word_p = words.pop(0)
-            for j in range(2):
-                for i in range(2):
+            for j in range(3):
+                for i in range(3):
                     for w in word_q:
                         exercise_words.append(w)
                 for k in range(j + 1):

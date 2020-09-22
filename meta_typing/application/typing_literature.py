e9bf2f703df7b68bc86cf99fb890d4516fd0c36a
Jonathan Wamsley <jonnywamsley@gmail.com>
2020-09-22 07:11:51 -0400

    added safety checks and documentation

--- meta_typing/application/typing_literature.py
+++ meta_typing/application/typing_literature.py
@@ -3,11 +3,26 @@ import os
 import glob
 
 class TypingLiterature:
-
+    '''
+    Actions
+    -------
+    TypingLiterature creates an interface so text or literature can be:
+        - typed
+            - user selects a file name
+            - returns string the text body of the file name
+        - added
+            - user types a file name
+            - user provides text body for file name
+            - the txt file is stored at MetaTyping/saved_literature/
+        - deleted
+            - user selects a file name
+            - the file is deleted
+    '''
     def __init__(self, stdscr):
         self.stdscr = stdscr
 
-    def start_up(self):
+    def __call__(self):
+        '''calls to perform actions until exit or type text is selected'''
         while True:
             action = self._literature_actions()
             text = self._perform_action(action)
@@ -35,28 +50,39 @@ class TypingLiterature:
 
     def _type_text(self):
         file_name = self._get_available_text()
-        text = self._get_literature(file_name)
-        return text
+        return self._get_literature(file_name) if file_name else None
+        
 
     def _add_new_text(self):
-        title_message = 'First, Enter the Title of the Literature and F4 when done: '
-        title_window = TextWindow(self.stdscr, message = title_message, termination_trigger = 'f4')
-        file_name = title_window.get_output()
-
-        text_message = 'Now, Enter the Text for the Literature and F4 when done: '
-        text_window = TextWindow(self.stdscr, message = text_message, termination_trigger = 'f4')
-        content = text_window.get_output()
+        '''ask for a file name and a text body'''
+        while True:
+            title_message = 'First, Enter the Title of the Literature and F4 when done: '
+            title_window = TextWindow(self.stdscr, message = title_message, termination_trigger = 'f4')
+            file_name = title_window.get_output()
+            if file_name:
+                break
+        
+        while True:
+            text_message = 'Now, Enter the Text for the Literature and F4 when done: '
+            text_window = TextWindow(self.stdscr, message = text_message, termination_trigger = 'f4')
+            content = text_window.get_output()
+            if content:
+                break
         self._write_literature(file_name, content)
 
     def _get_available_text(self):
-        file_names = [file.replace('../saved_literature/', '').replace('.txt', '') for file in glob.glob('../saved_literature/*.txt')]
+        file_names = [file.replace('../saved_literature/', '').replace('.txt', '') 
+            for file in glob.glob('../saved_literature/*.txt')]
+        if not file_names:
+            return None
         file_names_window = SelectionWindow(self.stdscr, selection_list = file_names)
         selected_file_name = file_names_window.get_selected_response()
         return selected_file_name
 
     def _del_text(self):
         file_name = self._get_available_text()
-        self._delete_literature(file_name)
+        if file_name:
+            self._delete_literature(file_name)
 
     def _get_literature(self, file_name):
         with open(f'../saved_literature/{file_name}.txt', 'r') as f:        
